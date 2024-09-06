fb54c17354d7a23446390c9f58e3143f7e79abc5
kamel <egmel@bk.ru>
2024-09-06 22:34:57 +0300

    камит

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Enter array numbers");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] arr = input.split(" ");
+        double avg = 0;
+        int[] array = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            try{
+                array[i] = Integer.parseInt(arr[i]);
+            } catch (NumberFormatException e){
+                System.out.println("Not a number " + array[i] + " Retry");
+                return;
+            }
+        }
+        System.out.print("Array is ");
+        for (int i = 0; i < array.length;i++){
+            System.out.print(array[i] + " ");
+            avg += array[i];
+        }
+        System.out.println();
+        System.out.println("Average " + avg/array.length);
+    }
+}
